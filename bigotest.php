fc95e964db196578dce652a5d7a7a13506001482
Kester <ugoh2kelechi@yahoo.com>
2016-06-04 19:27:49 -0400

    big o notation
    finding the lowest number in an array

--- bigotest.php
+++ bigotest.php
@@ -1,7 +1,37 @@
 <?php
 //Big O notation test
 
+// Finding the lowest number in an array of numbers.
 
+
+Class BigOnotation {
+
+
+	public function getMinNums($arrayofnums)
+	{
+		$curmin = $arrayofnums[0];
+
+		for($x=1; $x <10; $x++)
+		{
+			if($arrayofnums[$x] < $curmin)
+			{
+				$curmin = $arrayofnums[$x];
+			}
+		}
+		return $curmin;
+	}
+
+}
+
+
+
+$bigo = new BigOnotation();
+
+$arraynums = [23,34,8,4,90,67,73,28,39,28,7,5,83];
+
+$min_nums = $bigo->getMinNums($arraynums);
+
+echo "The lowest number in the array is: ".$min_nums;
 
 
 
